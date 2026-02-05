47768361adbc0996ecfda2e61ad4c6ec7be9f885
adilbek <adilbekarman4@gmail.com>
2026-02-05 18:14:18 +0500

    add deploy script (p.s сразу два деплоя)

---
+++ scripts/deploy.js
@@ -0,0 +1,27 @@
+const hre = require("hardhat");
+
+async function main() {
+  //деплой токена
+  const ArtToken = await hre.ethers.getContractFactory("ArtToken");
+  const token = await ArtToken.deploy();
+  await token.waitForDeployment();
+  const tokenAddress = await token.getAddress();
+  console.log("ArtToken deployed to:", tokenAddress);
+
+  //деплой основного контракта
+  const ArtLaunch = await hre.ethers.getContractFactory("ArtLaunch");
+  const launch = await ArtLaunch.deploy(tokenAddress);
+  await launch.waitForDeployment();
+  const launchAddress = await launch.getAddress();
+  console.log("ArtLaunch deployed to:", launchAddress);
+
+
+  const tx = await token.setMinter(launchAddress);
+  await tx.wait();
+  console.log("Minter set to ArtLaunch contract");
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+})
