f73ab2f6eafc2d64ab296d3f83e70f195bbdef23
adilbek <adilbekarman4@gmail.com>
2026-02-05 15:38:04 +0500

    update contract address

--- contracts/ArtToken.sol
+++ contracts/ArtToken.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: Adil
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -10,13 +10,13 @@ contract ArtToken is ERC20 {
         minter = msg.sender;
     }
 
-    // Позволяет только основному контракту чеканить токены [cite: 39, 42]
+    // позволяет только основному контракту чеканить токены 
     function mint(address to, uint256 amount) external {
         require(msg.sender == minter, "Only ArtLaunch can mint");
         _mint(to, amount);
     }
 
-    // Передача прав на чеканку контракту ArtLaunch
+    // передача прав на чеканку контракту ArtLaunch
     function setMinter(address _newMinter) external {
         require(msg.sender == minter, "Not authorized");
         minter = _newMinter;
