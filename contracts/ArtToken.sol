bc8bc1dfcfce4350a56041803f6a81114702e833
adilbek <adilbekarman4@gmail.com>
2026-02-06 13:38:11 +0500

    add Arttoken with minting logic for rewards

--- contracts/ArtToken.sol
+++ contracts/ArtToken.sol
@@ -1,6 +1,5 @@
-// SPDX-License-Identifier: Adil
-pragma solidity >=0.7.0 <0.9.0;
-
+// SPDX-License-Identifier: Adilbekk
+pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ArtToken is ERC20 {
@@ -9,14 +8,10 @@ contract ArtToken is ERC20 {
     constructor() ERC20("ArtLaunch Token", "ART") {
         minter = msg.sender;
     }
-
-    // позволяет только основному контракту чеканить токены 
     function mint(address to, uint256 amount) external {
         require(msg.sender == minter, "Only ArtLaunch can mint");
         _mint(to, amount);
     }
-
-    // передача прав на чеканку контракту ArtLaunch
     function setMinter(address _newMinter) external {
         require(msg.sender == minter, "Not authorized");
         minter = _newMinter;
