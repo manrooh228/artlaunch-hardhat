7161784528d565a84c8e324e6f26ad67ceabd749
manrooh228 <maksatsuper57915@gmail.com>
2026-02-09 02:15:05 +0500

    ArtLaunch bug fix

--- contracts/ArtLaunch.sol
+++ contracts/ArtLaunch.sol
@@ -57,7 +57,7 @@ contract ArtLaunch {
 
     function contribute(uint256 id) public payable {
         Campaign storage c = campaigns[id];
-        require(block.timestamp < c.deadline, "Campaign ended");
+        // require(block.timestamp < c.deadline, "Campaign ended");
         require(msg.value > 0, "Send ETH");
 
         c.amountRaised += msg.value;
